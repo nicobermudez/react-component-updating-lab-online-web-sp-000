46b229198c9395b1244bb8f87d22d9b95623becd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 15:09:56 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -12,7 +12,10 @@ class Timer extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
   }
 
   componentDidUpdate() {
